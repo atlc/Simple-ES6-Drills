4607cfa85bd4932717be41911108924ad9f8f898
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-17 06:35:09 -0500

    Partially began changing functions into arrow functions

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
     
-    function favoriteMovie(movie = 'The Room', name = 'world') {
-        console.log(`My name is ${name} & my favorite movie is ${movie}`);
-    }
+    let favoriteMovie = (movie = 'The Room', name = 'world') => console.log(`My name is ${name} & my favorite movie is ${movie}`);
 
     favoriteMovie();
     favoriteMovie('Leprechaun 6: Back 2 tha Hood');
     favoriteMovie('Leprechaun 6: Back 2 tha Hood', 'Andrew');
+
+     let getFirstName = (name) => {
+
+     }
+
+     let getFirstNameConcise = name => console.log();
 });
